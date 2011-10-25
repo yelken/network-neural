6093545a79d0f2ee6d1b5815d17b17b4e3bc407e
yelkezin <yelkezin@7fe28781-deab-6890-8369-fa1bd6347cb6>
2011-10-25 04:22:02 +0000

    Corrigir alguns Bugs referentes ao treinamento e calculo do resultado

--- network-neural/src/fsm/com/br/Main.java
+++ network-neural/src/fsm/com/br/Main.java
@@ -1,11 +1,23 @@
 package fsm.com.br;
 
+import java.util.Vector;
+
 import fsm.com.br.controller.Sinapse;
+import fsm.com.br.model.Neuronio;
+import fsm.com.br.model.Utils;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Sinapse sinapse = new Sinapse();
-		sinapse.treinarRede();
+		
+		Integer[] teste = {Utils.bia, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 1, 1, 1, 1, 1 };
+		
+		Vector<Neuronio> aux =  sinapse.treinarRede();
+		
+		
+		System.out.println(sinapse.calcularResultado(teste, aux));
+		
 	}
 }
